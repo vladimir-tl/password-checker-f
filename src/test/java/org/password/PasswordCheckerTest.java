1a7fb75c31e66d2fc0b72298a502a2bbc2c3713e
Marija <gestuverteja.marija@gmail.com>
2024-02-18 16:26:33 +0200

    updated the las project pom file in MAIN

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,6 +1,8 @@
 package org.password;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordCheckerTest {
@@ -8,5 +10,26 @@ public class PasswordCheckerTest {
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
          assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
+    }
+    @Test
+    void shouldReturnFalseNotMeetingLengthAndAdditionalRequirements(){
+        assertFalse(PasswordChecker.checkPasswordComplexity("short"));
+    }
+    @Test
+    void shouldReturnFalseNotMeetingOnlyLengthRequirements() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Short1@"));
+    }
+
+    @Test
+    void shouldReturnFalseForMissingDigitalCharacters(){
+        assertFalse(PasswordChecker.checkPasswordComplexity("Password@"));
+    }
+    @Test
+    void shouldReturnFalseForMissingSpecialCharacters() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Password1"));
+    }
+    @Test
+    void shouldReturnFalseForMissingBothSpecialCharactersAndDigits() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Password"));
     }
 }
---
+++ src/test/readme.md
@@ -0,0 +1,8 @@
+| Number | Checklist                                                                                 | Status |
+|:------:|:------------------------------------------------------------------------------------------|:------:|
+|   1    | Verify password complexity compliance with all requirements                               |  Pass  |
+|   2    | Verify minimum character limit without special or digit characters                        |  Pass  |
+|   3    | Verify minimum character limit with special and digit characters                          |  Pass  |
+|   4    | Verify prevention of password submission with missing digital characters                  |  Pass  |
+|   5    | Verify prevention of password submission with missing special characters                  |  Pass  |
+|   6    | Verify prevention of password submission with missing both digital and special characters |  Pass  |
